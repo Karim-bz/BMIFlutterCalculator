bcf381afcc702b81a6164e89fe14c891d355bae1
karimbouzid <kaarimbouzid@gmail.com>
2020-11-29 23:44:21 +0100

    Main Screen

--- lib/Screens/mainPage.dart
+++ lib/Screens/mainPage.dart
@@ -1,5 +1,6 @@
 import 'dart:ffi';
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 class MainPage extends StatelessWidget {
   @override
@@ -37,7 +38,74 @@ class _MainPageViewState extends State<MainPageView> {
             margin: EdgeInsets.all(15.0),
             child: Column(
               children: [
-                twoRows(),
+                Row(
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.symmetric(vertical: 20),
+                      child: ButtonTheme(
+                        child: Container(
+                          margin: EdgeInsets.fromLTRB(0, 0, 30, 0),
+                          height: 150,
+                          width: 150,
+                          child: RaisedButton(
+                            padding: EdgeInsets.fromLTRB(0, 20, 0, 0),
+                            color: const Color(0xff111328),
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(10),
+                            ),
+                            child: Column(
+                              children: [
+                                Icon(
+                                  FontAwesomeIcons.mars,
+                                  color: Colors.white,
+                                  size: 60,
+                                ),
+                                Text(
+                                  "MALE",
+                                  style: TextStyle(
+                                      color: Colors.grey,
+                                      fontSize: 20,
+                                      height: 2),
+                                ),
+                              ],
+                            ),
+                            onPressed: () {},
+                          ),
+                        ),
+                      ),
+                    ),
+                    ButtonTheme(
+                      child: Container(
+                        height: 150,
+                        width: 150,
+                        child: RaisedButton(
+                          padding: EdgeInsets.fromLTRB(0, 20, 0, 0),
+                          color: const Color(0xff111328),
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(10),
+                          ),
+                          child: Column(
+                            children: [
+                              Icon(
+                                FontAwesomeIcons.venus,
+                                color: Colors.white,
+                                size: 60,
+                              ),
+                              Text(
+                                "MALE",
+                                style: TextStyle(
+                                    color: Colors.grey,
+                                    fontSize: 20,
+                                    height: 2),
+                              ),
+                            ],
+                          ),
+                          onPressed: () {},
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
                 Container(
                   decoration: BoxDecoration(
                     color: const Color(0xff111328),
@@ -52,16 +120,29 @@ class _MainPageViewState extends State<MainPageView> {
                         style: TextStyle(
                             color: Colors.grey, fontSize: 20, height: 2),
                       ),
-                      Text(
-                        "$_value Cm",
-                        style: TextStyle(
-                            color: Colors.white, fontSize: 30, height: 1.4),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        crossAxisAlignment: CrossAxisAlignment.end,
+                        children: [
+                          Text(
+                            "$_value",
+                            style: TextStyle(
+                                color: Colors.white,
+                                fontSize: 30,
+                                height: 1.4,
+                                fontWeight: FontWeight.bold),
+                          ),
+                          Text(
+                            "cm",
+                            style: TextStyle(
+                                color: Colors.grey, fontSize: 15, height: 2),
+                          ),
+                        ],
                       ),
                       Slider(
                         value: _value.toDouble(),
                         min: 1.0,
                         max: 300.0,
-                        // divisions: 10,
                         activeColor: Colors.pinkAccent,
                         inactiveColor: Colors.white,
                         label: '_value',
@@ -70,54 +151,160 @@ class _MainPageViewState extends State<MainPageView> {
                             _value = newValue.round();
                           });
                         },
-                        semanticFormatterCallback: (double newValue) {
-                          return '${newValue.round()} dollars';
-                        },
                       ),
                     ],
                   ),
                 ),
-                twoRows(),
-                Container(
-                  decoration: BoxDecoration(
-                    color: Colors.pink,
-                    borderRadius: BorderRadius.circular(0),
+                Row(
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.symmetric(vertical: 20),
+                      child: Container(
+                        margin: EdgeInsets.fromLTRB(0, 0, 30, 0),
+                        decoration: BoxDecoration(
+                          color: const Color(0xff111328),
+                          borderRadius: BorderRadius.circular(10),
+                        ),
+                        height: 150,
+                        width: 150,
+                        child: Column(
+                          children: [
+                            Text(
+                              "Weight",
+                              style: TextStyle(
+                                  color: Colors.grey, fontSize: 20, height: 2),
+                            ),
+                            Text(
+                              "Value",
+                              style: TextStyle(
+                                  color: Colors.white,
+                                  fontSize: 24,
+                                  fontWeight: FontWeight.bold),
+                            ),
+                            Row(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: [
+                                ButtonTheme(
+                                  minWidth: 20.0,
+                                  child: RaisedButton(
+                                    color: Colors.pink,
+                                    textColor: Colors.white,
+                                    shape: RoundedRectangleBorder(
+                                      borderRadius: BorderRadius.circular(50.0),
+                                    ),
+                                    child: Text(
+                                      "-",
+                                      style: TextStyle(
+                                          fontWeight: FontWeight.bold),
+                                    ),
+                                    onPressed: () {},
+                                  ),
+                                ),
+                                ButtonTheme(
+                                  minWidth: 20.0,
+                                  child: RaisedButton(
+                                    color: Colors.pink,
+                                    textColor: Colors.white,
+                                    shape: RoundedRectangleBorder(
+                                      borderRadius: BorderRadius.circular(50.0),
+                                    ),
+                                    child: Text(
+                                      "+",
+                                      style: TextStyle(
+                                          fontWeight: FontWeight.bold),
+                                    ),
+                                    onPressed: () {},
+                                  ),
+                                )
+                              ],
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                    Container(
+                      decoration: BoxDecoration(
+                        color: const Color(0xff111328),
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                      height: 150,
+                      width: 150,
+                      child: Column(
+                        children: [
+                          Text(
+                            "AGE",
+                            style: TextStyle(
+                                color: Colors.grey, fontSize: 20, height: 2),
+                          ),
+                          Text(
+                            "Value",
+                            style: TextStyle(
+                                color: Colors.white,
+                                fontSize: 24,
+                                fontWeight: FontWeight.bold),
+                          ),
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              ButtonTheme(
+                                minWidth: 20.0,
+                                child: RaisedButton(
+                                  color: Colors.pink,
+                                  textColor: Colors.white,
+                                  shape: RoundedRectangleBorder(
+                                    borderRadius: BorderRadius.circular(50.0),
+                                  ),
+                                  child: Text(
+                                    "-",
+                                    style:
+                                        TextStyle(fontWeight: FontWeight.bold),
+                                  ),
+                                  onPressed: () {},
+                                ),
+                              ),
+                              ButtonTheme(
+                                minWidth: 20.0,
+                                child: RaisedButton(
+                                  color: Colors.pink,
+                                  textColor: Colors.white,
+                                  shape: RoundedRectangleBorder(
+                                    borderRadius: BorderRadius.circular(50.0),
+                                  ),
+                                  child: Text(
+                                    "+",
+                                    style:
+                                        TextStyle(fontWeight: FontWeight.bold),
+                                  ),
+                                  onPressed: () {},
+                                ),
+                              )
+                            ],
+                          )
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
+                FlatButton(
+                  color: Colors.pink,
+                  textColor: Colors.white,
+                  minWidth: double.infinity,
+                  splashColor: Colors.pinkAccent,
+                  padding: EdgeInsets.all(15),
+                  onPressed: () {
+                    /*...*/
+                  },
+                  child: Text(
+                    "CALCULATE",
+                    style:
+                        TextStyle(fontSize: 35.0, fontWeight: FontWeight.bold),
                   ),
-                  height: 70,
-                  width: double.infinity,
                 ),
               ],
             ),
           ),
         ),
       ),
-    );
-  }
-
-  Row twoRows() {
-    return Row(
-      children: [
-        Padding(
-          padding: const EdgeInsets.symmetric(vertical: 20),
-          child: Container(
-            margin: EdgeInsets.fromLTRB(0, 0, 30, 0),
-            decoration: BoxDecoration(
-              color: const Color(0xff111328),
-              borderRadius: BorderRadius.circular(10),
-            ),
-            height: 150,
-            width: 150,
-          ),
-        ),
-        Container(
-          decoration: BoxDecoration(
-            color: const Color(0xff111328),
-            borderRadius: BorderRadius.circular(10),
-          ),
-          height: 150,
-          width: 150,
-        ),
-      ],
     );
   }
 }
